dc700de3423b31e802d3d32022c302755362589a
thompson <xmumug@gmail.com>
2019-09-13 22:01:05 +0800

    umei.fun 需自行获取登陆cookie

--- umei/app.py
+++ umei/app.py
@@ -4,13 +4,14 @@ from bs4 import BeautifulSoup
 from time import sleep
 
 urls = ['https://www.umei.fun/categories/16?page={}'.format(str(i)) for i in range(1,63)]
+cookie = ''
 
 headers = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
     'accept-encoding': 'gzip, deflate, br',
     'accept-language': 'zh-CN,zh;q=0.9',
     'cache-control': 'max-age=0',
-    'cookie': '',
+    'cookie': cookie,
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
 }
 
