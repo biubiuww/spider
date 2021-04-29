ea02dd155ebc41078ea4a30062ef5e6575a497b9
Biubiu <xmumug@gmail.com>
2021-04-29 23:21:12 +0800

    添加翻页解析

--- 91user/user.py
+++ 91user/user.py
@@ -3,6 +3,7 @@ import urllib.parse as up
 from time import sleep
 from bs4 import BeautifulSoup
 
+
 headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'Accept-Encoding': 'gzip, deflate',
@@ -31,47 +32,56 @@ class User():
 
     def __init__(self,uid):
         self.uid = uid
-        #self.baseurl = 'http://91porn.com/uvideos.php?UID='
+        self.start_url = 'http://91porn.com/uvideos.php?UID={}'.format(self.uid)
+        self.num = self.__page_num()
+
+    def __page_num(self):
+        page = get_page(self.start_url)
+        page_num = page.select('ul.nav.navbar-nav.navbar-right > a')[-1].get_text()
+        num = re.findall(r'[0-9]\d',page_num)[0]
+        flag = int(num) // 8
+        if flag == 0:
+            flag = 1
+            return flag
+        elif flag == 1:
+            return flag
+        else:
+            flag += 2
+            return flag
+
 
     def __parse_user(self):
-        url = 'http://91porn.com/uvideos.php?UID=' + str(self.uid)
-        page = get_page(url)
-        video_urls = page.select('div.well.well-sm > a')
-        video_ids = page.select('div.thumb-overlay > img')
-        video_names = page.select('span.video-title.title-truncate.m-t-5')
-        for url,id,name in zip(video_urls,video_ids,video_names):
-            data = {
-                'url':url.get('href'),
-                'id':id.get('src').split('/')[-1].strip('.jpg'),
-                'name':name.get_text()
-            }
-            yield data
+        urls = ['http://91porn.com/uvideos.php?UID={}&page={}'.format(str(self.uid),str(num))for num in range(1,int(self.num))]
+        # page = get_page(self.start_url)
+        for url in urls:
+            print(url)
+            page = get_page(url)
+            video_urls = page.select('div.well.well-sm > a')
+            video_ids = page.select('div.thumb-overlay > img')
+            video_names = page.select('span.video-title.title-truncate.m-t-5')
+            for url,id,name in zip(video_urls,video_ids,video_names):
+                data = {
+                    'url':url.get('href'),
+                    'id':id.get('src').split('/')[-1].strip('.jpg'),
+                    'title':name.get_text()
+                }
+                yield data
 
     def parse_video(self):
-        #user_data = self.__parse_user()
+        data_list = []
         for user_data in self.__parse_user():
             page = get_page(user_data['url'])
-            # page = get_page('http://91porn.com/view_video.php?viewkey=2f081f01590d54ccaaee')
-            # video_m3u8 = page.find_all('script')
             m3u8 = page.find(text=re.compile('.*"%.*"'))
             temp =  m3u8.split('"')[-2]
             m3u8_url = up.unquote(temp).split("'")[1]
             new_data = {
                 'url': user_data['url'],
                 'id': user_data['id'],
-                'title': user_data['name'],
+                'title': user_data['title'],
                 'm3u8': m3u8_url
             }
-            sleep(2)
-            print(new_data)
-            # return new_data
-
-
-'''
-http://91porn.com/uvideos.php?UID=877aSjDOejaLC0iTIMIi6bhbHa56usJpAZE7gUUrYCButroR&type=public
-'''
-test = User('fe7dCN6lNv5VirM8tSKWVndvRtHMSVyeHRRNQDEbKvUfjKzE')
-# test.uid = 'fe7dCN6lNv5VirM8tSKWVndvRtHMSVyeHRRNQDEbKvUfjKzE'
-test.parse_video()
-
+            sleep(1)
+            data_list.append(new_data)
+        # return new_data
+        print(data_list)
 
