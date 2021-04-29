cbad3e94ba18b4624a59af49a12c43a786cb15d0
Biubiu <xmumug@gmail.com>
2021-04-30 00:26:58 +0800

    bug fix

--- 91user/user.py
+++ 91user/user.py
@@ -1,25 +1,39 @@
 import requests, re
+import random
 import urllib.parse as up
 from time import sleep
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
-
-headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'zh-CN,zh;q=0.9',
-    'Cache-Control': 'max-age=0',
-    'Cookie': '__cfduid=dda7b976a0a240beb0968fd6673951c471618894642; CLIPSHARE=jkaaau6k1p151iqhto35fsgrnl; mode=d',
-    'Host': '91porn.com',
-    'Referer': 'http://91porn.com/uvideos.php?UID=', #fe7dCN6lNv5VirM8tSKWVndvRtHMSVyeHRRNQDEbKvUfjKzE
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36'
-}
+ua = UserAgent()
 proxies = {
   'http': 'http://127.0.0.1:1080',
   'https': 'http://127.0.0.1:1080'
 }
+
+def random_headers():
+    ip = str(random.choice(list(range(255)))) + '.' + str(random.choice(list(range(255)))) + '.' + str(
+        random.choice(list(range(255)))) + '.' + str(random.choice(list(range(255))))
+
+    headers = {
+        'X-Client-IP': ip,
+        'X-Remote-IP': ip,
+        'X-Remote-Addr': ip,
+        'X-Originating-IP': ip,
+        'x-forwarded-for': ip,
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Cache-Control': 'max-age=0',
+        # 'Cookie': '__cfduid=dda7b976a0a240beb0968fd6673951c471618894642; CLIPSHARE=jkaaau6k1p151iqhto35fsgrnl; mode=d',
+        'Host': '91porn.com',
+        'Referer': 'http://91porn.com/',  # fe7dCN6lNv5VirM8tSKWVndvRtHMSVyeHRRNQDEbKvUfjKzE
+        'User-Agent': ua.random
+    }
+    return headers
+
 def get_page(url):
-    page = requests.get(url,headers=headers,proxies=proxies)
+    page = requests.get(url,headers=random_headers(),proxies=proxies)
     page.encoding = page.apparent_encoding
     html = BeautifulSoup(page.text,'lxml')
     if html != None:
@@ -80,7 +94,7 @@ class User():
                 'title': user_data['title'],
                 'm3u8': m3u8_url
             }
-            sleep(1)
+            # sleep(1)
             data_list.append(new_data)
         # return new_data
         print(data_list)
